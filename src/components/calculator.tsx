ddd3cb1f6771496b401b4c636311fede6a42e583
IsmaelJefferson <ismael_jefferson181@hotmail.com>
2022-03-05 17:31:03 -0300

    feat: add calculator app

---
+++ src/components/calculator.tsx
@@ -0,0 +1,26 @@
+import {useState} from 'react';
+import '../styles/styles.css'
+
+export function Calculator(){
+    const [numberOne, setNumberOne] =  useState<string>('0');
+    const [numberTwo, setNumberTwo] =  useState<string>('0');
+    const [result, setResult] =  useState<number>(0);
+
+
+    function sum(numOne: string ,numTwo:string){
+        return setResult(parseFloat(numOne) + parseFloat(numTwo));
+    };
+
+    return (
+        <div className="container">
+            <h1>{process.env.REACT_APP_APP_NAME}</h1>
+
+            <input type="number" min={0}  name="numberOne" value={numberOne} onChange={(event) => setNumberOne(event.target.value)}/> 
+            <p id="plusSing">+</p>
+            <input type="number" min={0} name="numberTwo" value={numberTwo} onChange={(event) => setNumberTwo(event.target.value)} /> <br />
+
+            <button onClick={() => sum(numberOne, numberTwo)}>Somar</button>
+            <h1 id="result">{result}</h1>
+        </div>
+    );
+}
